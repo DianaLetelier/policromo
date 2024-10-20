15c99fdbd0e355d4695ae15b986bcf7e36d2e830
DianaLetelier <dialeteliers@gmail.com>
2024-10-20 09:26:20 -0300

    Solución de errores y verificación de estructura

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+
+const App = () => {
+  return <h1>Bienvenidos a Policromo</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ src/styles/README.md
@@ -0,0 +1,2 @@
+# Policromo
+Proyecto para la difusión de artistas y estilos musicales.
